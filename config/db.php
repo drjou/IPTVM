c90f7196ae8d3aa8f32707b5dda068792c2c5715
LazarAngelov <nkcslxh@126.com>
2016-10-14 11:14:19 +0800

    add yii framework basic

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=iptvm',
+    'username' => 'kuanhong',
+    'password' => 'kuanhongiptvm',
+    'charset' => 'utf8',
+];
